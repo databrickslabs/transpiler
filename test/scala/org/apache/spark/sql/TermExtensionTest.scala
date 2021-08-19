30f8be420138153a2c45d9292e4bce6cbd8b212b
Serge Smertin <serge.smertin@databricks.com>
2021-08-19 14:24:08 +0200

    Initial support for `TERM('str')` expansion
    * Should transform `TERM('str')` into matching all string columns against `str`: e.g. `SELECT * FROM (SELECT a, b, c FROM x) WHERE TERM("pizza") OR TERM("pasta")` to `SELECT * FROM (SELECT a, b, c FROM x) WHERE (a = "pizza" OR b = "pizza" OR c = "pizza") OR (a = "pasta" OR b = "pasta" OR c = "pasta")`
    * implementation is oversimplified and would be refined in subsequent commits

---
+++ test/scala/org/apache/spark/sql/TermExtensionTest.scala
@@ -0,0 +1,33 @@
+package org.apache.spark.sql
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers._
+
+case class Dummy(a: String, b: String, c: String, n: Int, valid: Boolean)
+
+class TermExtensionTest extends AnyFunSuite {
+  val dummy = Seq(
+    Dummy("a", "b", "c", 1, valid = true),
+    Dummy("d", "e", "f", 2, valid = false),
+    Dummy("g", "h", "i", 3, valid = true),
+    Dummy("h", "g", "f", 4, valid = false),
+    Dummy("e", "d", "c", 5, valid = true),
+  )
+
+  test("it filters") {
+    val spark = SparkSession.builder()
+      .withExtensions(e => new TermExtension().apply(e))
+      .master("local[1]")
+      .getOrCreate()
+    import spark.implicits._
+
+    val result = spark.createDataset(dummy)
+      .where("TERM('e') OR term('a')")
+    result.show()
+
+    val rows = result.select("n").as[Int].collect()
+    rows mustEqual Seq(1, 2, 5)
+  }
+}
+
+object x
