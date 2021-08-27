a702e70544bf098cc19eae73d4186b842577e508
Serge Smertin <serge.smertin@databricks.com>
2021-08-27 13:37:22 +0200

    Linked some of documentation

--- src/main/scala/spl/ast.scala
+++ src/main/scala/spl/ast.scala
@@ -20,6 +20,11 @@ case class Alias(expr: Expr, name: String) extends Expr with Field
 
 sealed trait Command
 case class SearchCommand(expr: Expr) extends Command
+
+/**
+ * @link https://docs.splunk.com/Documentation/SplunkCloud/8.2.2106/Search/Usetheevalcommandandfunctions
+ * @link https://docs.splunk.com/Documentation/Splunk/8.2.2/SearchReference/CommonEvalFunctions
+ */
 case class EvalCommand(fields: Seq[(Value,Expr)]) extends Command
 case class FieldConversion(func: String, field: Value, alias: Option[Value])
 case class ConvertCommand(timeformat: Option[String], convs: Seq[FieldConversion]) extends Command
@@ -28,6 +33,33 @@ case class LookupCommand(options: Map[String,String], dataset: String, fields: S
 case class CollectCommand(args: Map[String,String], fields: Seq[Value]) extends Command
 case class WhereCommand(expr: Expr) extends Command
 case class TableCommand(fields: Seq[Value]) extends Command
-case class StatsCommand(params: Map[String, String], funcs: Seq[Expr], by: Seq[Value] = Seq(), dedupSplitVals: Boolean = false) extends Command
+
+/**
+ * Documentation taken from:
+ * @link https://docs.splunk.com/Documentation/SplunkCloud/8.2.2106/SearchReference/Stats
+ * @param params
+ *               [partitions=<num>] If specified, partitions the input data based on the split-by fields for
+ *               multithreaded reduce. The partitions argument runs the reduce step (in parallel reduce processing)
+ *               with multiple threads in the same search process on the same machine. Compare that with parallel
+ *               reduce, using the redistribute command, that runs the reduce step in parallel on multiple machines.
+ *               [allnum=<bool>] If true, computes numerical statistics on each field if and only if all of the values
+ *               of that field are numerical.
+ *               [delim=<string>] Specifies how the values in the list() or values() aggregation are delimited.
+ * @param funcs  See Stats function options. The function can be applied to an eval expression,
+ *               or to a field or set of fields. Use the AS clause to place the result into
+ *               a new field with a name that you specify. You can use wild card characters
+ *               in field names. For more information on eval expressions, see Types of eval
+ *               expressions in the Search Manual.
+ * @param by The name of one or more fields to group by. You cannot use a wildcard character to
+ *           specify multiple fields with similar names. You must specify each field separately.
+ *           The BY clause returns one row for each distinct value in the BY clause fields. If no
+ *           BY clause is specified, the stats command returns only one row, which is the aggregation
+ *           over the entire incoming result set.
+ * @param dedupSplitVals Specifies whether to remove duplicate values in multivalued BY clause fields.
+ */
+case class StatsCommand(params: Map[String, String],
+                        funcs: Seq[Expr],
+                        by: Seq[Value] = Seq(),
+                        dedupSplitVals: Boolean = false) extends Command
 
 case class Pipeline(commands: Seq[Command])
