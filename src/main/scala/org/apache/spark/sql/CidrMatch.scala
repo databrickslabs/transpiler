78fce49749ce6eafaec562e31be44329ed86b83b
Thomas Bonfert <87115450+tombonfert@users.noreply.github.com>
2021-11-26 14:51:51 +0100

    Finalise CIDR searching (#56)

--- src/main/scala/org/apache/spark/sql/CidrMatch.scala
+++ src/main/scala/org/apache/spark/sql/CidrMatch.scala
@@ -1,53 +1,49 @@
 package org.apache.spark.sql
 
 import org.apache.spark.sql.catalyst.expressions.{Add, And, Cast, ElementAt, Expression, ExpressionDescription, GreaterThanOrEqual, LessThanOrEqual, Literal, Multiply, Pow, RuntimeReplaceable, StringSplit, SubstringIndex, Subtract}
-import org.apache.spark.sql.types.{BooleanType, DataType, IntegerType}
+import org.apache.spark.sql.types.{DoubleType, IntegerType, StringType}
 
 @ExpressionDescription(
   usage = "_FUNC_(cidr, ip) - Matches IP address string with the supplied CIDR string",
   since = "3.3.0")
 case class CidrMatch(cidr: Expression, ip: Expression) extends RuntimeReplaceable {
-  override def nullable: Boolean = false
-  override def dataType: DataType = BooleanType
-
-  override def exprsReplaced: Seq[Expression] = Seq(cidrMatch)
-  override def child: Expression = cidr // TODO: is this correct?..
+  override def child: Expression = cidrMatch
+  override def exprsReplaced: Seq[Expression] = Seq(cidr, ip)
+  override def flatArguments: Iterator[Any] = Iterator(cidr, ip)
 
   // TODO: add special handling for /8, /16, and /24 with StartsWith()
   private def cidrMatch = And(
     GreaterThanOrEqual(ipAddress, lowAddress),
     LessThanOrEqual(ipAddress, highAddress))
-
   private def ipAddress: Add = aton(ip)
   private def lowAddress: Add = aton(SubstringIndex(cidr, Literal.create("/"), Literal.create(1)))
   private def highAddress: Add = Add(lowAddress, numAddresses)
 
   private def numAddresses = Subtract(
-    Pow(
-      Literal.create(2),
+    Cast(Pow(
+      Literal.create(2.0),
       Subtract(
-        Literal.create(32),
-        SubstringIndex(
+        Literal.create(32.0),
+        Cast(SubstringIndex(
           cidr,
           Literal.create("/"),
           Literal.create(-1)
-        )
+        ), DoubleType)
       )
-    ), Literal.create(1))
+    ), IntegerType),
+    Literal.create(1))
 
   private def aton(addr: Expression): Add = {
-    val bytes = new StringSplit(addr, Literal.create("\\."))
+    val bytes = new StringSplit(Cast(addr, StringType), Literal.create("\\."))
     Add(Add(Add(
-      addrMult(bytes, 0, 256*256*256),
-      addrMult(bytes, 1, 256*256)),
-      addrMult(bytes, 2, 256)),
-      addrMult(bytes, 3, 1))
+      addrMult(bytes, 1, 256*256*256),
+      addrMult(bytes, 2, 256*256)),
+      addrMult(bytes, 3, 256)),
+      addrMult(bytes, 4, 1))
   }
 
   private def addrMult(bytes: Expression, offset: Int, multiple: Int) =
-    Cast(
       Multiply(
-        ElementAt(bytes, Literal.create(offset)),
-        Literal.create(multiple)),
-      IntegerType)
+        Cast(ElementAt(bytes, Literal.create(offset)), IntegerType),
+        Literal.create(multiple))
 }
