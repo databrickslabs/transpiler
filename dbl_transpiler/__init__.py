42126426259db9037ed621af3af355f4b94e3c12
Serge Smertin <serge.smertin@databricks.com>
2022-11-13 17:09:06 +0100

    Add build scripts

--- dbl_transpiler/__init__.py
+++ dbl_transpiler/__init__.py
@@ -2,13 +2,15 @@ def transpile(spl: str):
     """Transpiles a query into PySpark DataFrame DSL"""
     from pyspark.sql import SparkSession
     spark = SparkSession.getActiveSession()
-    code = spark._jvm.spl.Transpiler.toPython(spl)
+    spl = spark._jvm.com.databricks.labs.transpiler.spl
+    code = spl.Transpiler.toPython(spl)
     print(code)
 
 def spl(query: str, do_display=False) -> 'pyspark.sql.DataFrame':
     from pyspark.sql import SparkSession, DataFrame
     spark = SparkSession.getActiveSession()
-    jdf = spark._jvm.spl.Transpiler.toDataFrame(spark._jsparkSession, query)
+    spl = spark._jvm.com.databricks.labs.transpiler.spl
+    jdf = spl.Transpiler.toDataFrame(spark._jsparkSession, query)
     df = DataFrame(jdf, spark)
     if not do_display:
         return df
--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setup(
     version='0.4.0',
     packages=find_packages(exclude=['tests', 'tests.*']),
     tests_require=['pyspark==3.3.0'],
-    package_data={
-        'dbl-transpiler': ['lib/*.jar']
-    }
+    author='Databricks Labs',
+    description='SIEM-to-PySpark Transpiler',
+    url='https://github.com/databrickslabs/transpiler',
+    license = 'Databricks License',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
 )
